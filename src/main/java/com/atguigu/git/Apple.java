10123ac385176527bd9f53533bea02780766b96b
eclipse_user <yang@pride.com>
2018-11-09 15:59:38 +0800

    hot_fix

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -4,5 +4,6 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("hello git!");
 		System.out.println("hello git git!刚刚");
+		System.out.println("hot_fix");
 	}
 }
